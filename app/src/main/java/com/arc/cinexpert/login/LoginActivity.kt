71537dacfb61412eabd1f7efc57bc99ef27db776
aromerocollados03 <alejandroromerocollados.alu@iespacomolla.es>
2024-03-16 18:22:13 +0100

    Initial commit

---
+++ app/src/main/java/com/arc/cinexpert/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.arc.cinexpert.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.arc.cinexpert.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
