8b532b1a8abb01ecf533cd33690581cac2d28e28
AlejandroRomero <alejandroromerocollados.alu@iespacomolla.es>
2024-04-22 19:46:20 +0200

    Implementación del mapa con la API de Google Maps

--- app/src/main/java/com/arc/cinexpert/MapFragment.kt
+++ app/src/main/java/com/arc/cinexpert/MapFragment.kt
@@ -1,6 +1,36 @@
 package com.arc.cinexpert
 
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 
-class MapFragment : Fragment() {
+class MapFragment : Fragment(), OnMapReadyCallback {
+    private lateinit var mMap: GoogleMap
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_map, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
+        mapFragment?.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        // Configuraciones adicionales pueden ir aquí
+    }
 }
+
+
+
