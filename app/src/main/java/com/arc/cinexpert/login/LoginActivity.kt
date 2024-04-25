982de1828f0225c2d5c190cc27fd3911fbcef725
AlejandroRomero <alejandroromerocollados.alu@iespacomolla.es>
2024-04-25 19:22:10 +0200

    Login con google

--- app/src/main/java/com/arc/cinexpert/login/LoginActivity.kt
+++ app/src/main/java/com/arc/cinexpert/login/LoginActivity.kt
@@ -1,6 +1,5 @@
 package com.arc.cinexpert.login
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
@@ -8,8 +7,14 @@ import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.arc.cinexpert.MainActivity
 import com.arc.cinexpert.R
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
 class LoginActivity : AppCompatActivity() {
 
@@ -18,6 +23,8 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var edtContrasena: EditText
     private lateinit var btnIniciarSesion: Button
     private lateinit var txtRegistrarse: TextView
+    private lateinit var googleSignInClient: GoogleSignInClient
+    companion object { private const val RC_SIGN_IN = 9001 }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,6 +51,20 @@ class LoginActivity : AppCompatActivity() {
             // Navegar a RegistroActivity
             startActivity(Intent(this, RegisterActivity::class.java))
         }
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        // Botón para iniciar sesión con Google
+        val btnSignInGoogle = findViewById<Button>(R.id.btnGoogle)
+        btnSignInGoogle.setOnClickListener {
+            val signInIntent = googleSignInClient.signInIntent
+            startActivityForResult(signInIntent, RC_SIGN_IN)
+        }
     }
 
     private fun iniciarSesion(correo: String, contrasena: String) {
@@ -55,5 +76,35 @@ class LoginActivity : AppCompatActivity() {
                 Toast.makeText(this, "Error de inicio de sesión: ${tarea.exception?.message}", Toast.LENGTH_SHORT).show()
             }
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)
+                firebaseAuthWithGoogle(account.idToken!!)
+            } catch (e: ApiException) {
+                Toast.makeText(this, "Error de Google Sign In: ${e.localizedMessage}", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        autenticacion.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Inicio de sesión exitoso, navegar al MainActivity
+                    val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                    startActivity(intent)
+                    finish() // Finaliza esta actividad para que el usuario no pueda volver a ella
+                } else {
+                    // Si falla el inicio de sesión, mostrar mensaje al usuario.
+                    Toast.makeText(this, "Error de inicio de sesión: ${task.exception?.message}", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
